a8a6a97e2edb43033baa07ac244ce88ea2a4b020
Cerceis <48720021+Cerceis@users.noreply.github.com>
2022-10-17 14:31:26 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const reactive = (v) => {
+    return {
+        _value: v,
+        _el: [],
+        get value(){return this._value},
+        set value(v){
+            this._value = v;
+            for(let i = 0; i < this._el.length; i++)
+                this._el[i].textContent = this._value;
+        },
+        bind(el){
+            this._el.push(el);
+            //bind data according to element type.
+            switch(el.localName){
+                case "input":
+                    el.value = this._value;
+                    break;
+                default:
+                    el.textContent = this._value;
+            }    
+        }
+    }
+}
+
+//Define something
+const myValue = reactive(1);
+const myName = reactive("type something");
+
+myValue.bind(document.querySelector("#displaySection1"))
+myValue.bind(document.querySelector("#displaySection2"))
+myValue.bind(document.querySelector("#displaySection3"))
+
+
+myName.bind(document.querySelector("#inputDisplaySection"))
+myName.bind(document.querySelector("#inputSection"))
+
+// functions
+const addValue = () => {
+    myValue.value ++;
+}
+
+
+
+
+
+
